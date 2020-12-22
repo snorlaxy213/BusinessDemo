f69a778903d8f72de8d16ccca548f9e7e6fffa85
Jules <1658895307@qq.com>
2020-12-22 18:30:13 +0800

    file mo'n

---
+++ file-monitoring/src/main/java/com/remo/filemonitoring/FileMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.remo.filemonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileMonitoringApplication.class, args);
+	}
+
+}
---
+++ file-monitoring/src/test/java/com/remo/filemonitoring/FileMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remo.filemonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
